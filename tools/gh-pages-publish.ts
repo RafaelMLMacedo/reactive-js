14854e3c83eb8d97e9e3d983e35a76fe799e6f4a
Rafael Macedo <rafaelmacedo4@gmail.com>
2020-04-20 02:41:57 +0100

    chore: update gh-update-publish file

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -1,12 +1,13 @@
-const { cd, exec, echo, touch } = require("shelljs")
-const { readFileSync } = require("fs")
-const url = require("url")
+require('dotenv').config()
+const url = require('url')
+const { readFileSync } = require('fs')
+const { cd, exec, echo, touch } = require('shelljs')
 
 let repoUrl
-let pkg = JSON.parse(readFileSync("package.json") as any)
-if (typeof pkg.repository === "object") {
-  if (!pkg.repository.hasOwnProperty("url")) {
-    throw new Error("URL does not exist in repository section")
+let pkg = JSON.parse(readFileSync('package.json') as any)
+if (typeof pkg.repository === 'object') {
+  if (!pkg.repository.hasOwnProperty('url')) {
+    throw new Error('URL does not exist in repository section')
   }
   repoUrl = pkg.repository.url
 } else {
@@ -14,18 +15,20 @@ if (typeof pkg.repository === "object") {
 }
 
 let parsedUrl = url.parse(repoUrl)
-let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
+let repository = (parsedUrl.host || '') + (parsedUrl.path || '')
 let ghToken = process.env.GH_TOKEN
 
-echo("Deploying docs!!!")
-cd("docs")
-touch(".nojekyll")
-exec("git init")
-exec("git add .")
+if (ghToken === void 0) {
+  throw new Error('ghToken was not defined.')
+}
+
+echo('Deploying docs!!!')
+cd('docs')
+touch('.nojekyll')
+exec('git init')
+exec('git add .')
 exec('git config user.name "Rafael Macedo"')
-exec('git config user.email "wine.and.vine@zohomail.com"')
+exec('git config user.email "rafaelmacedo4@gmail.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
-exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
-)
-echo("Docs deployed!!")
+exec(`git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`)
+echo('Docs deployed!!')
